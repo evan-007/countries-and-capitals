629c0381084c741c0b83315fb2c1529f044cd0a8
Evan <eulloyd@gmail.com>
2014-06-19 06:26:06 +0000

    Add gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
 
 gulp.task('connect', function() {
 	connect.server({
@@ -7,4 +12,21 @@ gulp.task('connect', function() {
 	});
 });
 
+
+gulp.task('copy-html-files', function() {
+  gulp.src(['./app/**/*.html', '!./app/index.html'], {base: './app'})
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('usemin', function() {
+  gulp.src('./app/index.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat', rev()],
+      js: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
 gulp.task('default', ['connect']);
+
+gulp.task('build', ['copy-html-files', 'usemin']);
