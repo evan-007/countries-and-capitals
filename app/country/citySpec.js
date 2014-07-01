3b2e7e7747c779edb83b6f040afde499b488b56c
Evan <eulloyd@gmail.com>
2014-07-01 07:13:57 +0000

    Update tests

--- app/country/citySpec.js
+++ app/country/citySpec.js
@@ -1,6 +1,6 @@
 describe('cc-app', function(){
 	beforeEach(module('cc-app'));
-
+  
 	it('should load the correct controller and template', function(){
 		inject(function($route){
 			expect($route.routes['/countries/:id/:city'].controller).toBe('cityCtrl');
--- app/country/countrySpec.js
+++ app/country/countrySpec.js
@@ -1,10 +1,28 @@
 describe('country.js', function(){
 	beforeEach(module('cc-app'));
+  
+  beforeEach(function(){
+    module(function($provide){
+      $provide.factory('Countries', function(){
+        return object.value;
+      });
+    });
+  });
 
 	it('should load the correct template and controller', function(){
-		inject(function($route){
-			expect($route.routes['/countries/:id'].controller).toBe('countryCtrl');
-			expect($route.routes['/countries/:id'].templateUrl).toBe('./country/country.html');
+		inject(function($httpBackend, $location, $rootScope, $route){
+      var id = 'FR';
+      $httpBackend.expectGET('/country.html').respond('...');
+      
+      $rootScope.$apply(function(){
+        $location.path('/countries/' + id);
+      });
+      
+      $httpBackend.flush();
+			$httpBackend.verifyNoOutstandingRequest();
+      
+			expect($route.current.controller).toBe('countryCtrl');
+			expect($route.current.templateUrl).toBe('./country/country.html');
 		});
 	});
 });
