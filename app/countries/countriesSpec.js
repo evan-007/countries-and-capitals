6160d0126a42f76f5e55652a7b755f732ac28f2f
Evan <eulloyd@gmail.com>
2014-07-02 06:27:43 +0000

    Add tests for countries route

--- app/countries/countriesSpec.js
+++ app/countries/countriesSpec.js
@@ -1,10 +1,39 @@
 describe('countries.js', function(){
+  
+  var fakeData = {
+    countriesDouble: function(){
+      return [1,2,3]
+    }
+  }
+  
 	beforeEach(module('cc-app'));
+  
+  beforeEach(function(){
+    
+    spyOn(fakeData, 'countriesDouble');
+    
+    module(function($provide){
+      $provide.factory('Countries', function(){
+        return fakeData.countriesDouble;
+      });
+    });
+  });
 
 	it('should load the correct controller and template', function(){
-		inject(function($route){
-			expect($route.routes['/countries'].controller).toBe('countriesCtrl');
-			expect($route.routes['/countries'].templateUrl).toBe('./countries/countries.html');
+		inject(function($httpBackend, $location, $route, $rootScope){
+      
+      $httpBackend.expectGET(/\.countries\/countries.html/).respond('...');
+      
+      $rootScope.$apply(function(){
+        $location.path('/countries');
+      });
+      
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingExpectations();
+      
+      expect($route.current.country).toBe('someCtrl');
+      expect($route.current.templateUrl).toBe('someTemplate');
+      expect(fakeData.countriesDouble).toHaveBeenCalled();
 		});
 	});
 });
